493da09c6240a5bdb75444c875addb72c38370d7
Elangovan920 <melangovan920@gmail.com>
2022-07-20 19:07:14 +0530

    elango code

--- src/org/bike/Bike.java
+++ src/org/bike/Bike.java
@@ -4,16 +4,21 @@ public class Bike {
 	 public static void main(String[] args) {
 		int a=0;
 		int b=0;
-		int num=15;
+		int num=12;
 		for(int i=2;i<num;i++) {
 			if(num%i==0) {
 				System.out.println("not a prime ");
+				
 				b++;
 				break;
 			}
 		}
          if(b==0) {
         	 System.out.println("prime");
+         
+         }
+         else {
+        	 System.out.println("not a prime");
          }
 }
 }
